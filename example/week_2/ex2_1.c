7a4d5042b288facff2c9a0c28b9c4d6b574c8b94
Scof <32575460+songyw0517@users.noreply.github.com>
2022-01-09 00:56:33 +0900

    Create ex2_1.c

---
+++ example/week_2/ex2_1.c
@@ -0,0 +1,21 @@
+/*
+새 파일 열고 닫기
+*/
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+int main(void) {
+    int fd;
+    mode_t mode;
+    mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH; // 권한 : 644
+    fd = open("unix.txt", O_CREAT, mode);
+    if (fd == -1) {
+        perror("Creat");
+        exit(1);
+        }
+        close(fd);
+        return 0;
+}
