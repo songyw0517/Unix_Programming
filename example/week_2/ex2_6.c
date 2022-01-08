144d818ef33b3ef4c6082dc013da0c2c10aa34c3
Scof <32575460+songyw0517@users.noreply.github.com>
2022-01-09 03:04:51 +0900

    Create ex2_6.c

---
+++ example/week_2/ex2_6.c
@@ -0,0 +1,32 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+int main(void) {
+    int fd, n;
+    off_t start, cur;
+    char buf[256];
+    fd = open("unix.txt", O_RDONLY); // 읽기 전용으로 읽음
+
+    // 제대로 읽었는지 확인
+    if (fd == -1) {
+        perror("Open unix.txt");
+        exit(1);
+    }
+
+    start = lseek(fd, 0, SEEK_CUR);
+    n = read(fd, buf, 255);
+    buf[n] = '\0';
+    printf("Offset start=%d, Read Str=%s, n=%d\n", (int)start, buf, n);
+
+    cur = lseek(fd, 0, SEEK_CUR);
+    printf("Offset cur=%d\n", (int)cur);
+    
+    start = lseek(fd, 5, SEEK_SET);
+    n = read(fd, buf, 255);
+    buf[n] = '\0';
+    printf("Offset start = %d, Read Str=%s", (int)start, buf);
+
+    close(fd);
+    return 0;
+}
